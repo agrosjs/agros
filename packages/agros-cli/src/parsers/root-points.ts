f13c89c7c9be12464c0a4ba530591d4356476194
lenconda <i@lenconda.top>
2022-06-23 09:55:54 +0800

    feat: 🎸 find root points

--- packages/agros-cli/src/parsers/root-points.ts
+++ packages/agros-cli/src/parsers/root-points.ts
@@ -1,7 +1,17 @@
 import * as path from 'path';
 import * as fs from 'fs-extra';
 import * as parser from '@babel/parser';
-import { ExpressionStatement, ImportDeclaration, ObjectProperty } from '@babel/types';
+import {
+    ExpressionStatement,
+    ImportDeclaration,
+    ObjectExpression,
+    ObjectProperty,
+} from '@babel/types';
+
+export interface ImportedItem {
+    localName: string;
+    source: string;
+}
 
 export const parseRootPoints = (entryPathname = 'index.ts') => {
     const content = fs.readFileSync(path.resolve(entryPathname)).toString();
@@ -55,5 +65,32 @@ export const parseRootPoints = (entryPathname = 'index.ts') => {
         return false;
     }) as ExpressionStatement;
 
-    const bootstrapArrayDeclaration: ObjectProperty[] = (callerDeclaration.expression as any)?.arguments[0]?.elements as ObjectProperty[] || [];
+    const moduleNames: string[] = ((callerDeclaration.expression as any)?.arguments[0]?.elements as any[] || [])
+        .filter((element) => element.type === 'ObjectExpression')
+        .map((element: ObjectExpression) => {
+            const moduleProperty: ObjectProperty = (element?.properties || [])
+                .find((property: ObjectProperty) => {
+                    return (property.key as any).name === 'module';
+                }) as ObjectProperty;
+            if (moduleProperty) {
+                return (moduleProperty.value as any).name;
+            }
+            return null;
+        })
+        .filter((moduleName) => !!moduleName);
+
+    const importedItems: ImportedItem[] = body.filter((statement) => {
+        return statement.type === 'ImportDeclaration';
+    }).reduce((result, currentStatement: ImportDeclaration) => {
+        return result.concat(currentStatement.specifiers.map((specifier) => {
+            return {
+                localName: specifier.local.name,
+                source: currentStatement.source.value,
+            };
+        }));
+    }, [] as ImportedItem[]).filter((importedItem) => {
+        return moduleNames.indexOf(importedItem.localName) !== -1;
+    });
+
+    return importedItems;
 };
