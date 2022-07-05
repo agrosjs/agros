7dfa8311a2f31fc66d89ee0e3355956a125b6144
lenconda <i@lenconda.top>
2022-07-05 22:13:46 +0800

    feat: 🎸 generate decorator function

---
+++ packages/agros-common/src/generate-decorator-code.ts
@@ -0,0 +1,19 @@
+import generate from '@babel/generator';
+import * as t from '@babel/types';
+import prettier from 'prettier';
+import * as path from 'path';
+import { ProjectConfigParser } from '@agros/config';
+
+export const generateDecoratorCode = async (ast: t.Node): Promise<string> => {
+    const projectConfigParser = new ProjectConfigParser();
+    const code = await prettier.format(
+        generate(ast as any).code + '\nclass AgrosPlaceholder {}',
+        {
+            parser: 'babel-ts',
+            tabWidth: projectConfigParser.getConfig('indentSize'),
+            plugins: [path.resolve(__dirname, '../node_modules/prettier-plugin-multiline-arrays')],
+            filepath: '',
+        },
+    );
+    return code.split(/\r|\n|\r\n/).slice(0, -2).join('\n');
+};
